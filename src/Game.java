a7414681342ee82a074147f7e058219ea2899dc6
Sam <samkaveh@outlook.com>
2025-07-30 10:11:41 +1000

    Got scoring working and moved event handler to Game class

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,8 @@
 package src;
 
 import java.awt.Image;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.util.Random;
 import javax.swing.*;
@@ -10,9 +12,12 @@ public class Game {
     private String[] flagPaths;
     private String assetsDir;
     private long gameSeed;
+    private int score;
 
     private JFrame rootFrame;
     private JLabel flagLabel;
+    private JTextField answerField;
+    private JLabel scoreLabel;
     
     public void run() {
         setupGUI();
@@ -24,6 +29,7 @@ public class Game {
         this.assetsDir = getAssetsDir();
         this.flagPaths = getFlagPaths();
         this.gameSeed = System.currentTimeMillis();
+        this.score = 0;
 
         // Root Window
         this.rootFrame = new JFrame("Flag Quiz");
@@ -34,16 +40,26 @@ public class Game {
         this.rootFrame.add(gamePanel);
 
         // Components
+        this.scoreLabel = new JLabel(Integer.toString(this.score));
+
         this.flagLabel = new JLabel();
         nextFlag(this.gameSeed);
 
         JLabel answerLabel = new JLabel("Answer:");
-        JTextField answerField = new JTextField(20);
-        JButton answerButton = new JButton("Submit");
+        this.answerField = new JTextField(20);
 
+        JButton answerButton = new JButton("Submit");
+        answerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                submitHandler();
+            }
+        });
+
+        gamePanel.add(this.scoreLabel);
         gamePanel.add(this.flagLabel);
         gamePanel.add(answerLabel);
-        gamePanel.add(answerField);
+        gamePanel.add(this.answerField);
         gamePanel.add(answerButton);
     }
 
@@ -62,5 +78,26 @@ public class Game {
         ImageIcon flagImage = new ImageIcon(this.assetsDir + this.flagPaths[randIdx]);
         flagImage.setImage(flagImage.getImage().getScaledInstance(400, 300, Image.SCALE_SMOOTH));
         this.flagLabel.setIcon(flagImage);
+    }
+
+    private void submitHandler() {
+        String answer = this.answerField.getText();
+        String prepAnswer = prepareAnswer(answer);
+        String currentFlag = this.flagLabel.getIcon().toString();
+        String prepCurrentFlag = currentFlag.substring(currentFlag.length() - prepAnswer.length());
+
+        if (prepareAnswer(answer).contentEquals(prepCurrentFlag)) {
+            System.out.println("Right Answer");
+            this.score++;
+            this.scoreLabel.setText(Integer.toString(this.score));
+            nextFlag(this.gameSeed);
+        }
+
+        this.answerField.setText("");
+    }
+
+    private String prepareAnswer(String answer) {
+        String prep_answer = answer.replace(" ", "_").toLowerCase();
+        return prep_answer + ".png";
     }
 }
