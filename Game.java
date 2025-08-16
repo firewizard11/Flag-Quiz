8c36cc971e830f29104dc93152296ab2ce641ffc
sam <samkaveh@outlook.com>
2025-08-16 19:07:43 +1000

    Made the code more OOP like

--- Game.java
+++ Game.java
@@ -2,41 +2,54 @@ import java.util.*;
 import java.io.*;
 
 public class Game {
-    public static void main(String[] args) {
-        // Initialize Game
-        int score = 0;
-        int roundCounter = 0;
-        String[] flagPaths = new File("./flags/").list();
-        int roundMax = flagPaths.length;
-        long seed = System.currentTimeMillis();
-        Random genIdx = new Random(seed);
-        Scanner userAnswer = new Scanner(System.in);
+    private int score;
+    private int roundCounter;
+    private int roundMax;
+    private Random genIdx;
+    private String[] flagPaths;
 
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.gameInit();
+        game.gameRun();
+    }
+
+    private void gameInit() {
+        long seed = System.currentTimeMillis();
+
+        this.score = 0;
+        this.roundCounter = 0;
+        this.flagPaths = new File("./flags/").list();
+        this.roundMax = flagPaths.length;
+        this.genIdx = new Random(seed);
+    }
+
+    private void gameRun() {
         String currentFlag;
         String answer;
+        Scanner userAnswer = new Scanner(System.in);
 
-        // Game Loop
         while (true) {
-            currentFlag = flagPaths[genIdx.nextInt(roundMax)];
+            currentFlag = this.flagPaths[this.genIdx.nextInt(roundMax)];
 
-            System.out.println("Score: " + score);
+            System.out.println("Score: " + this.score);
             System.out.println("currentFlag: " + currentFlag);
 
             System.out.print("Answer: ");
             answer = userAnswer.nextLine();
 
             if (answer.compareTo(currentFlag) == 0) {
-                score++;
+                this.score++;
             }
 
-            roundCounter++;
+            this.roundCounter++;
 
-            if (roundCounter == roundMax) {
+            if (this.roundCounter == this.roundMax) {
                 break;
             }
         }
 
-        System.out.printf("Final Score: %d / %d\n", score, roundMax);
+        System.out.printf("Final Score: %d / %d\n", this.score, this.roundMax);
         userAnswer.close();
     }
 }
